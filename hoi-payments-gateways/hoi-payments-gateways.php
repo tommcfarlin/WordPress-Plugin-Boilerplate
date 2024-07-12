0f53eccc730acf6ee7be8657467fa5b1badadce7
msalsouri <msalsouri@hotmail.com>
2024-07-12 21:10:53 +0100

    Corrected plugin loader file references and function names

--- hoi-payments-gateways/hoi-payments-gateways.php
+++ hoi-payments-gateways/hoi-payments-gateways.php
@@ -1,68 +1,54 @@
 <?php
-
-/**
- * The plugin bootstrap file
- *
- * This file is read by WordPress to generate the plugin information in the plugin
- * admin area. This file also includes all of the dependencies used by the plugin,
- * registers the activation and deactivation functions, and defines a function
- * that starts the plugin.
- *
- * @link              http://example.com
- * @since             1.0.0
- * @package           Plugin_Name
- *
- * @wordpress-plugin
- * Plugin Name:       WordPress Plugin Boilerplate
- * Plugin URI:        http://example.com/hoi-payments-gateways-uri/
- * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
- * Version:           1.0.0
- * Author:            Your Name or Your Company
- * Author URI:        http://example.com/
- * License:           GPL-2.0+
- * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
- * Text Domain:       hoi-payments-gateways
- * Domain Path:       /languages
+/*
+  Plugin Name:       HOI Payments Gateways
+  Plugin URI:        https://hoiltd.com/hoi-payments-gateways-uri/
+  Description:       HOI WordPress Payments Gateways for Stripe, PayPal, and beyond.
+  Version:           1.0.0
+  Author:            Mohammad Al-Souri
+  Author URI:        https://msalsouri.github.io/
+  License:           GPL-2.0+
+  License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+  Text Domain:       hoi-payments-gateways
+  Domain Path:       /languages
  */
 
 // If this file is called directly, abort.
-if ( ! defined( 'WPINC' ) ) {
+if (!defined('WPINC')) {
 	die;
 }
 
 /**
- * Currently plugin version.
+ * The current plugin version.
  * Start at version 1.0.0 and use SemVer - https://semver.org
- * Rename this for your plugin and update it as you release new versions.
  */
-define( 'PLUGIN_NAME_VERSION', '1.0.0' );
+define('HOI_PAYMENTS_GATEWAYS_VERSION', '1.0.0');
 
 /**
  * The code that runs during plugin activation.
- * This action is documented in includes/class-hoi-payments-gateways-activator.php
+ * This action is documented in src/includes/class-hoi-payments-gateways-activator.php
  */
-function activate_plugin_name() {
-	require_once plugin_dir_path( __FILE__ ) . 'includes/class-hoi-payments-gateways-activator.php';
-	Plugin_Name_Activator::activate();
+function activate_hoi_payments_gateways() {
+	require_once plugin_dir_path(__FILE__) . 'src/includes/class-hoi-payments-gateways-activator.php';
+	Hoi_Payments_Gateways_Activator::activate();
 }
 
 /**
  * The code that runs during plugin deactivation.
- * This action is documented in includes/class-hoi-payments-gateways-deactivator.php
+ * This action is documented in src/includes/class-hoi-payments-gateways-deactivator.php
  */
-function deactivate_plugin_name() {
-	require_once plugin_dir_path( __FILE__ ) . 'includes/class-hoi-payments-gateways-deactivator.php';
-	Plugin_Name_Deactivator::deactivate();
+function deactivate_hoi_payments_gateways() {
+	require_once plugin_dir_path(__FILE__) . 'src/includes/class-hoi-payments-gateways-deactivator.php';
+	Hoi_Payments_Gateways_Deactivator::deactivate();
 }
 
-register_activation_hook( __FILE__, 'activate_plugin_name' );
-register_deactivation_hook( __FILE__, 'deactivate_plugin_name' );
+register_activation_hook(__FILE__, 'activate_hoi_payments_gateways');
+register_deactivation_hook(__FILE__, 'deactivate_hoi_payments_gateways');
 
 /**
  * The core plugin class that is used to define internationalization,
  * admin-specific hooks, and public-facing site hooks.
  */
-require plugin_dir_path( __FILE__ ) . 'includes/class-hoi-payments-gateways.php';
+require plugin_dir_path(__FILE__) . 'src/includes/class-hoi-payments-gateways.php';
 
 /**
  * Begins execution of the plugin.
@@ -73,10 +59,9 @@ require plugin_dir_path( __FILE__ ) . 'includes/class-hoi-payments-gateways.php'
  *
  * @since    1.0.0
  */
-function run_plugin_name() {
-
-	$plugin = new Plugin_Name();
+function run_hoi_payments_gateways() {
+	$plugin = new Hoi_Payments_Gateways();
 	$plugin->run();
-
 }
-run_plugin_name();
+run_hoi_payments_gateways();
+
